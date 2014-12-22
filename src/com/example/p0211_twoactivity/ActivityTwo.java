da845a5fb5f8c28abd4ad306463d7c5d26d2ebb7
Punk <2morrow2late@rambler.ru>
2014-12-23 00:44:57 +0100

    Two activities
    Simple example of using two activities

---
+++ src/com/example/p0211_twoactivity/ActivityTwo.java
@@ -0,0 +1,13 @@
+package com.example.p0211_twoactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityTwo extends Activity {
+
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	  }
+	
+}
